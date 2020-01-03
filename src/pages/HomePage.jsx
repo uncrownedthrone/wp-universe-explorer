0c5d8d3d919e29b74ab1eec7ea1a5cdf258c8cb0
Justin Swiger <justin.swiger@me.com>
2020-01-03 14:05:31 -0500

    map and api pulls working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 const HomePage = () => {
-  const [houses, setHouses] = useState([])
-  const getHousesData = async () => {
-    const resp = await axios.get('https://localhost:5001/api/House')
-    setHouses(resp.data)
-  }
-  useEffect(() => {
-    getHousesData()
-  }, [])
-
-  return (
-    <>
-      <header>All Hogwarts Houses</header>
-      {houses.map(house => {
-        return (
-          <>
-            <h3>{house.HouseName}</h3>
-          </>
-        )
-      })}
-    </>
-  )
+  return <div>THIS IS THE HOME PAGE</div>
 }
 
 export default HomePage
--- src/pages/HousePage.jsx
+++ src/pages/HousePage.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const HousePage = () => {
-  return <div>THIS IS THE HOUSE PAGE</div>
+const HousePage = props => {
+  const [houses, setHouses] = useState([])
+  const [houseName, setHouseName] = useState('')
+  const [houseColor, setHouseColor] = useState('')
+  const [houseId, setHouseId] = useState('')
+  const getHouse = async () => {
+    const resp = await axios.get(`https://localhost:5001/api/House`)
+    setHouses(resp.data)
+  }
+
+  const sendHouseToApi = async () => {
+    const resp = await axios.post('https://localhost:5001/api/House', {
+      houseName: houseName,
+      houseColor: houseColor,
+      houseId: props.match.params.id,
+    })
+
+    console.log(resp.data)
+    setHouses(prev => {
+      return {
+        ...prev,
+        houses: [...prev.houses.concat(resp.data)],
+      }
+    })
+  }
+
+  useEffect(() => {
+    getHouse()
+  }, [])
+
+  return (
+    <>
+      <section>
+        <header>New House</header>
+        <input
+          type="text"
+          value={houseName}
+          placeholder="Enter a House Name"
+          onChange={e => setHouseName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={houseColor}
+          placeholder="Enter a House Color"
+          onChange={e => setHouseColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={houseId}
+          placeholder="Enter a House ID #"
+          onChange={e => setHouseId(e.target.value)}
+        />
+        <button onClick={sendHouseToApi}>Create Hogwarts House</button>
+      </section>
+      <div>
+        <header>All Houses</header>
+        {houses.map(house => {
+          return (
+            <>
+              <h1>{house.houseName}</h1>
+              <h3>{house.houseColor}</h3>
+            </>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
 export default HousePage
